4275c895de6a565a5c310b8fd9ef635ffa2a431b
Cassie <cassierasugu@gmail.com>
2023-06-11 10:08:29 +0300

    add users.rb

---
+++ app/product.rb
@@ -0,0 +1,29 @@
+class Product < ActiveRecord::Base
+  # ...
+
+  def reviews
+    Review.where(product_id: id)
+  end
+
+  def users
+    User.joins(:reviews).where(reviews: { product_id: id })
+  end
+
+  def leave_review(user, star_rating, comment)
+    Review.create(user: user, product: self, star_rating: star_rating, comment: comment)
+  end
+
+  def print_all_reviews
+    reviews.each do |review|
+      puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+    end
+  end
+
+  def average_rating
+    total_ratings = reviews.sum(:star_rating)
+    total_reviews = reviews.count
+
+    average = total_reviews > 0 ? total_ratings.to_f / total_reviews : 0.0
+    average.round(2)
+  end
+end
---
+++ app/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+
+  def reviews
+    Review.where(user_id: id)
+  end
+
+  def products
+    Product.joins(:reviews).where(reviews: { user_id: id })
+  end
+
+  def favorite_product
+    reviews.order(star_rating: :desc).first&.product
+  end
+
+  def remove_reviews(product)
+    Review.where(user_id: id, product_id: product.id).destroy_all
+  end
+end
